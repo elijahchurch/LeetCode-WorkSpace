a6c7dc24f5f0e8368e8ef27636ec7dc2f2650a53
Jason E. Church <elijahchurch@gmail.com>
2024-01-15 16:25:38 -0800

    Streamline findWinners by adding LossesByPlayer object

--- 1-15.tsx
+++ 1-15.tsx
@@ -1,11 +1,17 @@
 function findWinners(matches: number[][]): number[][] {
-    let winners = [];
-    let losers = [];
+    let winners : number[] = [];
+    let losers : number[] = [];
+    let lossesByPlayer: Record<number, number> = {};
     matches.forEach(element => {
         winners.push(element[0]);
         losers.push(element[1]);
+        if(lossesByPlayer[element[1]]) {
+            lossesByPlayer[element[1]] = lossesByPlayer[element[1]] + 1;
+        } else {
+            lossesByPlayer[element[1]] = 1;
+        }
     })
-    const undefeated : number[] = winners.filter((e, index) => !losers.includes(e) && winners.indexOf(e) === index);
-    const oneLoss: number[] = losers.filter(e => losers.indexOf(e) === losers.lastIndexOf(e));
-    return [(undefeated.sort((a,b) => a - b)), (oneLoss.sort((a, b) => a -b))];
+    const undefeated : number[] = [... new Set(winners)].filter((e, index) => lossesByPlayer[e] === undefined);
+    const oneLoss: number[] = losers.filter(e => lossesByPlayer[e] === 1);
+    return [(undefeated.sort((a,b) => a - b)), (oneLoss.sort((a, b) => a - b))];
 };
