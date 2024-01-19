cf0e156f90264112a06ede8056791621e164b472
Jason E. Church <elijahchurch@gmail.com>
2024-01-19 12:50:12 -0800

    Pass test for splicing first element off array

--- leet-code-testing/src/_tests_/Minimum-Falling-Path-Sum.test.tsx
+++ leet-code-testing/src/_tests_/Minimum-Falling-Path-Sum.test.tsx
@@ -8,4 +8,12 @@ describe("minFallingPathSum and helper functions", () => {
         expect(result).toEqual("number");
     });
 
+    test("splice should correctly pull out the first index and leave a mutated array without the first index element", () => {
+        const testArray = [[1], [2]];
+        const firstElement = testArray.splice(0, 1);
+        expect(firstElement).toEqual([[1]]);
+        expect(testArray).toEqual([[2]]);
+    });
+
+    
 });
