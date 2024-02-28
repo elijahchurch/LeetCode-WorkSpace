a4c66984660f89b886dbda71f53f539436ad801e
Jason E. Church <elijahchurch@gmail.com>
2024-02-28 14:46:14 -0800

    Create final test to see if input nums array correctly mutates

--- leet-code-testing/src/_tests_/Remove-Duplicate-from-Sorted-Array.test.tsx
+++ leet-code-testing/src/_tests_/Remove-Duplicate-from-Sorted-Array.test.tsx
@@ -3,14 +3,20 @@ import { duplicateRemover } from "../Problems/Remove-Duplicates-from-Sorted-Arra
 describe("duplicateRemover", () => {
 
     test("duplicateRemover instantiates and returns a number", () => {
-        const number = duplicateRemover([1,23,4]);
+        const number = duplicateRemover([1, 23, 4]);
         const result = typeof number;
         expect(result).toEqual("number");
     });
 
     test("duplicateRemover should loop through num array and remove duplicates", () => {
-        const testArray = [0,1,1,1,2,2,3,4,4];
+        const testArray = [0, 1, 1, 1, 2, 2, 3, 4, 4];
         const result = duplicateRemover(testArray);
         expect(result).toEqual(5);
     })
+
+    test("testArray should only have unique element after duplicatRemover mutates it", () => {
+        const testArray = [0, 1, 1, 1, 2, 2, 3, 4, 4];
+        duplicateRemover(testArray);
+        expect(testArray).toEqual([0, 1, 2, 3, 4]);
+    });
 })
